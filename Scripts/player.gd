45161a6af218441d2a77806c39fb39addb2d4624
OrangePainting <krishkon4065@gmail.com>
2025-08-02 12:14:27 -0500

    Updated Tile Map
    12:14 8/2/25

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,31 +1,52 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
+
+const COYOTE_TIME = 0.075
+var coyote_timer = 0.0
+
+const JUMP_BUFFER_TIME = 0.1
+var jump_buffer_timer = 0.0
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.$AnimatedSprite2D
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Decrease coyote timer
+		if coyote_timer > 0:
+			coyote_timer -= delta
+	else:
+		# Reset coyote timer when on ground
+		coyote_timer = COYOTE_TIME
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Decrease jump buffer timer
+	if jump_buffer_timer > 0:
+		jump_buffer_timer -= delta
+
+	# Register jump input (buffered)
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer_timer = JUMP_BUFFER_TIME
+
+	# Attempt jump if within both jump buffer and coyote time
+	if jump_buffer_timer > 0 and coyote_timer > 0:
+		velocity.y = JUMP_VELOCITY
+		jump_buffer_timer = 0
+		coyote_timer = 0
+
+	# Get movement input
 	var direction := Input.get_axis("move left", "move right")
-	
-	# Gets the input direction: -1, 0, 1
+
+	# Flip sprite
 	if direction == 1:
 		animated_sprite.flip_h = false
 	elif direction == -1:
 		animated_sprite.flip_h = true
-		
-	# Play Animations
+
+	# Play animations
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
@@ -33,8 +54,8 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-	
-	# Apply movement
+
+	# Horizontal movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
