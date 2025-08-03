253f415a45db544733fc5823b0f1b11c500373ff
OrangePainting <krishkon4065@gmail.com>
2025-08-02 20:32:38 -0500

    Added Parallax Background
    20:32 8/2/25

---
+++ Scripts/cloud_layer.gd
@@ -0,0 +1,8 @@
+extends ParallaxLayer
+
+
+@export var CLOUD_SPEED: float = -15
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.motion_offset.x += CLOUD_SPEED * delta
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
+const SPEED = 450.0
 const JUMP_VELOCITY = -300.0
 
 const COYOTE_TIME = 0.1
